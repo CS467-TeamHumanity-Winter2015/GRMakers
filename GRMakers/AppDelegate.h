3d63ddf4d2aae5beaaed170b391bbb734ae0bc67
Chrisness <Chris@Chriss-MacBook-Air.local>
2015-01-28 13:56:43 -0500

    Initial Commit

---
+++ GRMakers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GRMakers
+//
+//  Created by Chris Mahoney on 1/28/15.
+//  Copyright (c) 2015 Chris Mahoney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GRMakers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GRMakers
+//
+//  Created by Chris Mahoney on 1/28/15.
+//  Copyright (c) 2015 Chris Mahoney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
