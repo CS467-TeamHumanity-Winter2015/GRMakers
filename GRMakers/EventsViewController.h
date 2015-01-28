c74e7d62ef28263c6defdfb40e89a8f8d5a5e427
Chrisness <Chris@Chriss-MacBook-Air.local>
2015-01-28 14:02:03 -0500

    First Commit

---
+++ GRMakers/EventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsViewController.h
+//  GRMakers
+//
+//  Created by Chris Mahoney on 1/28/15.
+//  Copyright (c) 2015 Chris Mahoney. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface EventsViewController : ViewController
+
+@end
---
+++ GRMakers/EventsViewController.m
@@ -0,0 +1,37 @@
+//
+//  EventsViewController.m
+//  GRMakers
+//
+//  Created by Chris Mahoney on 1/28/15.
+//  Copyright (c) 2015 Chris Mahoney. All rights reserved.
+//
+
+#import "EventsViewController.h"
+
+@interface EventsViewController ()
+
+@end
+
+@implementation EventsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
